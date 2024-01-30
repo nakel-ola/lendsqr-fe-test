1cd73b3d96a09c6ef39e40f8e282d9711ffbe4b5
Olamilekan <nunuolamilekan@gmail.com>
2024-01-30 04:52:32 +0100

    refactor: Updated users list

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,13 +1,21 @@
 import users from "@/data/users.json";
+import { NextResponse } from "next/server";
 
 type Context = {
   params: {
     id: string;
   };
 };
-export async function GET(request: Request, context: Context) {
-  const id = context.params.id;
-  const user = users.find((user) => user.id.toString() === id);
 
-  return Response.json(user);
+export async function GET(request: Request, context: Context) {
+  try {
+    const id = context.params.id;
+    const user = users.find((user) => user.id.toString() === id);
+
+    if (!user) throw new Error("Not found");
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,32 +4,36 @@ import users from "@/data/users.json";
 import { isSameDay } from "date-fns";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  try {
+    const { searchParams } = new URL(request.url);
 
-  const organization = searchParams.get("organization");
-  const email = searchParams.get("email");
-  const username = searchParams.get("username");
-  const phone_number = searchParams.get("phone_number");
-  const status = searchParams.get("status");
-  const joined_at = searchParams.get("joined_at");
-  const page = searchParams.get("page") ?? 1;
-  const pageSize = searchParams.get("pageSize") ?? 10;
+    const organization = searchParams.get("organization");
+    const email = searchParams.get("email");
+    const username = searchParams.get("username");
+    const phone_number = searchParams.get("phone_number");
+    const status = searchParams.get("status");
+    const joined_at = searchParams.get("joined_at");
+    const page = searchParams.get("page") ?? 1;
+    const pageSize = searchParams.get("pageSize") ?? 10;
 
-  let filteredUsers: User[] = (users as User[]).filter((user) => {
-    return (
-      (!organization || user.organization.includes(organization)) &&
-      (!username || user.username.includes(username)) &&
-      (!email || user.email.includes(email)) &&
-      (!phone_number || user.phone_number.includes(phone_number)) &&
-      (!status || user.status === status) &&
-      (!joined_at || isSameDay(new Date(user.joined_at), new Date(joined_at)))
-    );
-  });
+    let filteredUsers: User[] = (users as User[]).filter((user) => {
+      return (
+        (!organization || user.organization.includes(organization)) &&
+        (!username || user.username.includes(username)) &&
+        (!email || user.email.includes(email)) &&
+        (!phone_number || user.phone_number.includes(phone_number)) &&
+        (!status || user.status === status) &&
+        (!joined_at || isSameDay(new Date(user.joined_at), new Date(joined_at)))
+      );
+    });
 
-  const newUsers = removeDuplicates(filteredUsers, "id");
+    const newUsers = removeDuplicates(filteredUsers, "id");
 
-  return Response.json({
-    totalItems: newUsers.length,
-    results: getUsersWithPagination(newUsers, Number(page), Number(pageSize)),
-  });
+    return Response.json({
+      totalItems: newUsers.length,
+      results: getUsersWithPagination(newUsers, Number(page), Number(pageSize)),
+    });
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 }
