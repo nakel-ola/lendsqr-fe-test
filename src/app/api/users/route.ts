2e6150b5fafcb954adb15e6bbdc3dd26c8ca8ea0
Olamilekan <nunuolamilekan@gmail.com>
2024-01-30 13:34:58 +0100

    refactor: Converted users api route to dynamic

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,6 +3,8 @@ import { User, getUsersWithPagination } from "@/utils/users";
 import users from "@/data/users.json";
 import { isSameDay } from "date-fns";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
